73f3a61610ff7264e5bf14b1d591a793ddc0b16c
growkingscripts <growkingscriptswebsite@gmail.com>
2024-09-26 23:48:22 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+fetch('https://raw.githubusercontent.com/growkingscripts/growkingscripts.github.io/refs/heads/main/assets/json/resellers.json')
+    .then(response => response.json())
+    .then(data => {
+		const resellerList = document.getElementById('resellerList');
 
+		data.datas.forEach(reseller => {
+			let div = document.createElement("div");
+			div.classList.add("numbered-item");
+			div.innerHTML = 'Username [Discord ID]: ' + reseller.username + ' [' + reseller.userid + ']';
+			resellerList.appendChild(div);
+		});
+	});	
