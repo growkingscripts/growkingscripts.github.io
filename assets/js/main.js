a1791b0abca267fb12168e4d712a08f3505439ff
growkingscripts <growkingscriptswebsite@gmail.com>
2024-09-26 23:45:41 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,7 +138,20 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-	
+
+fetch('https://raw.githubusercontent.com/growkingscripts/growkingscripts.github.io/refs/heads/main/assets/json/resellers.json')
+    .then(response => response.json())
+    .then(data => {
+		const resellerList = document.getElementById('resellerList');
+
+		data.datas.forEach(reseller => {
+			let div = document.createElement("div");
+			div.classList.add("numbered-item");
+			div.innerHTML = 'Username [Discord ID]: ' + reseller.username + ' [' + reseller.userid + ']';
+			resellerList.appendChild(div);
+		});
+	});	
+
 (function() {
   "use strict";
 
