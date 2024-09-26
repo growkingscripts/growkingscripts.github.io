efb11b5e0a7ca5dc6baf46f9b150a6bb1ee490c8
growkingscripts <growkingscriptswebsite@gmail.com>
2024-09-26 23:44:55 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,1 @@
-// Ambil data dari file JSON
-fetch('https://raw.githubusercontent.com/')
-    .then(response => response.json())
-    .then(data => {
-		const resellerList = document.getElementById('resellerList');
 
-		// Iterasi melalui data dan tambahkan ke list
-		data.datas.forEach(reseller => {
-			let div = document.createElement("div");
-			div.classList.add("numbered-item");
-			div.innerHTML = 'Username [Discord ID]: ' + reseller.username + ' [' + reseller.userid + ']';
-			resellerList.appendChild(div);
-		});
-	});
